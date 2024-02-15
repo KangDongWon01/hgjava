58c764c9266e88718d446e7056df62629508d77b
admin <admin@DESKTOP-C59RQQG>
2024-02-15 17:46:04 +0900

    2015 개인프로젝트

--- kdw_project/src/com/kdw/DiaryMethods.java
+++ kdw_project/src/com/kdw/DiaryMethods.java
@@ -1,23 +1,93 @@
 package com.kdw;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class DiaryMethods {
 	static Scanner scn = new Scanner(System.in);
+	Connection conn;
+	String sql;
+	PreparedStatement psmt;
+	ResultSet rs;
+	
+	void disconn() {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+			if(psmt != null) {
+				psmt.close();
+			}
+			if(rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	//로그인 메소드
 	public boolean Login(String user_id, String user_pw) {
-		System.out.println("로그인 화면입니다.");
-		System.out.println("아이디를 입력하세요. >>");
-		String id = scn.nextLine();
-		Connection conn = DiaryDAO.getConn();
-		sql = "select user_id from d_user where user_id = ? "
-		return 
-
+		conn = DiaryDAO.getConn();
+		sql = "select user_id, user_pw from d_user where user_id = ? and user_pw = ?";
+		try {
+				PreparedStatement psmt = conn.prepareStatement(sql);
+				psmt.setString(1, user_id);
+				psmt.setString(2, user_pw);
+				rs = psmt.executeQuery();
+				while(rs.next()) {
+					if(rs.getString("user_id").equals(user_id) && rs.getString("user_pw").equals(user_pw)) {
+						return true;
+					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				disconn();
+			}
+			return false;
 	}
 	//회원가입 메소드
-	
+	public boolean join(String user_id, String user_pw, String user_name, Date user_bd) {
+		conn = DiaryDAO.getConn();
+		
+		return false;
+	}
 	//CRUD 메소드
 	
+	//일기 작성 메소드
+	public boolean insertDiary(int diary_id, String title, String cont, String regDate, String updateDate ) throws SQLException {
+		//쿼리문작성.
+		conn = DiaryDAO.getConn(); //Connection 객체 생성.
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		sql = "insert into diary (DIARY_ID, TITLE, CONT, REGDATE, UPDATEDATE) VALUE(SEQ_DIARY_ID.NEXTVAL,?,?,SYSDATE,? )";
+		
+		try {
+			DiaryObject doj = new DiaryObject(); 
+			psmt = conn.prepareStatement(sql, new String[] {"diary_id"}); //데이터베이스 아직 시퀀스 안줬다. 참고하자
+			psmt.setString(1, doj.getTitle());
+			psmt.setString(2, doj.getCont());
+			psmt.setString(3, sdf.format(doj.getUpdateDate()));
+			
+			int r = psmt.executeUpdate(); //데이터 반영.
+			if(r > 0) {
+				return true;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disconn();
+		}
+		return false;
+	}
+	
+//	public boolean deleteDiary()
 }
--- kdw_project/src/com/kdw/DiaryObject.java
+++ kdw_project/src/com/kdw/DiaryObject.java
@@ -1,6 +1,45 @@
 package com.kdw;
 
+import java.util.Date;
+
 public class DiaryObject {
 	//다이어리 객체를 생성하기 위한 라이브러리형 클래스
+	private int diary_id;
+	private String title;
+	private String cont;
+	private Date regDate;
+	private Date updateDate;
+	
+	
+	public int getDiary_id() {
+		return diary_id;
+	}
+	public void setDiary_id(int diary_id) {
+		this.diary_id = diary_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCont() {
+		return cont;
+	}
+	public void setCont(String cont) {
+		this.cont = cont;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
 	
 }
